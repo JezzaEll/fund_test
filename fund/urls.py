a9e4eecf598f76128dfe69ac5ae9d8703d164364
JezzaEll <elliott.jelliott@gmail.com>
2024-03-07 10:14:21 +0000

    Change views to class based

--- fund/urls.py
+++ fund/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import fund_list, fund_upload, fund_api_list, fund_api_detail
+from .views import FundListView, FundUploadView, FundAPIListView, FundAPIDetailView
 
 urlpatterns = [
-    path('list/', fund_list, name='fund_list'),
-    path('upload/', fund_upload, name='fund_upload'),
-    path('api/list/', fund_api_list, name='fund_api_list'),
-    path('api/detail/<int:fund_id>/', fund_api_detail, name='fund_api_detail'),
+    path('list/', FundListView.as_view(), name='fund_list'),
+    path('upload/', FundUploadView.as_view(), name='fund_upload'),
+    path('api/list/', FundAPIListView.as_view(), name='fund_api_list'),
+    path('api/detail/<int:fund_id>/', FundAPIDetailView.as_view(), name='fund_api_detail'),
 ]
--- fund/views.py
+++ fund/views.py
@@ -1,32 +1,38 @@
-from django.shortcuts import render
-
 from django.shortcuts import render, redirect
+from django.views import View
 from django.http import JsonResponse
 from django.db.models import Sum
 from .models import Fund
 from .forms import FundUploadForm
-
 from csv import DictReader
 from io import TextIOWrapper
-
 import csv
 
-def fund_list(request):
-    strategy = request.GET.get('strategy', '')
-    funds = Fund.objects.filter(strategy=strategy) if strategy else Fund.objects.all()
-    strategies = Fund.objects.all().order_by('strategy').values_list('strategy', flat=True).distinct()
-    total_aum = funds.aggregate(Sum('aum'))
-    context = {'funds': funds, 'total_aum': total_aum['aum__sum'], 'strategies': strategies}
-    return render(request, 'fund/fund_list.html', context)
+class FundListView(View):
+    template_name = 'fund/fund_list.html'
 
-def fund_upload(request):
-    if request.method == 'POST':
+    def get(self, request):
+        print(type(request))
+        strategy = request.GET.get('strategy', '')
+        funds = Fund.objects.filter(strategy=strategy) if strategy else Fund.objects.all()
+        strategies = Fund.objects.all().order_by('strategy').values_list('strategy', flat=True).distinct()
+        total_aum = funds.aggregate(Sum('aum'))
+        context = {'funds': funds, 'total_aum': total_aum['aum__sum'], 'strategies': strategies}
+        return render(request, self.template_name, context)
+
+class FundUploadView(View):
+    template_name = 'fund/fund_upload.html'
+
+    def get(self, request):
+        form = FundUploadForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
         form = FundUploadForm(request.POST, request.FILES)
         if form.is_valid():
             csv_file = request.FILES['csv_file'].read().decode('utf-8-sig').splitlines()
             csv_reader = csv.DictReader(csv_file)
             for row in csv_reader:
-                print(row)
                 # check if fund already exists
                 if not Fund.objects.filter(name=row['Name']).exists():
                     # Create new fund
@@ -50,19 +56,18 @@ def fund_upload(request):
                     if row['Inception Date'] != '':
                         fund.inception_date = row['Inception Date']
                     fund.save()
-                    
             return redirect('fund_list')
-    else:
-        form = FundUploadForm()
-    return render(request, 'fund/fund_upload.html', {'form': form})
+        return render(request, self.template_name, {'form': form})
 
-def fund_api_list(request):
-    strategy = request.GET.get('strategy', '')
-    funds = Fund.objects.filter(strategy=strategy) if strategy else Fund.objects.all()
-    data = [{'id': fund.id, 'name': fund.name, 'strategy': fund.strategy, 'aum': fund.aum} for fund in funds]
-    return JsonResponse(data, safe=False)
+class FundAPIListView(View):
+    def get(self, request):
+        strategy = request.GET.get('strategy', '')
+        funds = Fund.objects.filter(strategy=strategy) if strategy else Fund.objects.all()
+        data = [{'id': fund.id, 'name': fund.name, 'strategy': fund.strategy, 'aum': fund.aum} for fund in funds]
+        return JsonResponse(data, safe=False)
 
-def fund_api_detail(request, fund_id):
-    fund = Fund.objects.get(id=fund_id)
-    data = {'id': fund.id, 'name': fund.name, 'strategy': fund.strategy, 'aum': fund.aum}
-    return JsonResponse(data)
+class FundAPIDetailView(View):
+    def get(self, request, fund_id):
+        fund = Fund.objects.get(id=fund_id)
+        data = {'id': fund.id, 'name': fund.name, 'strategy': fund.strategy, 'aum': fund.aum}
+        return JsonResponse(data)
